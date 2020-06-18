a143092631b80033ba0f3be72dc991b58e4adec2
VanoVasilyan <vanik199702@bk.ru>
2020-06-18 14:28:29 +0400

    circle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+    // constructor(){
+    //     super();
+        state = {
+            isDisabled:true,
+            count:0
+        }
+    // }
+    increment = () =>{
+            this.setState({
+                count: this.state.count - 1
+            })
+            if(this.state.count === 1){
+                this.setState({
+                    isDisabled : true
+                })
+            }
+    }
+    decrement = () =>{
+        if(this.state.isDisabled) this.setState({isDisabled:false})
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    render(){
+        return (
+            <div className = "main">
+            <button className = "myButton" onClick = {this.increment} disabled = {this.state.isDisabled}>&#8592;</button>
+        <p className = "div">{
+            this.state.count
+        }</p>
+            <button className = "myButton" onClick = {this.decrement}>&#8594;</button>
+            </div>
+        )
+    }
 }
+
+
+
 
 export default App;
